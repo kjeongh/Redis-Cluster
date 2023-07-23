e0420706c1fe67704f3cd163216bbc54b6b3a199
jhkim <wellsy001012@gmail.com>
2023-07-23 19:37:34 +0900

    cluster 구성 파일

---
+++ cluster-create.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# Redis 클러스터 구성
+docker exec -it redis-master-1 redis-cli --cluster create redis-master-1:7001 redis-master-2:7002 redis-master-3:7003 &&
+# Slave 노드 추가
+docker exec -it redis-master-1 redis-cli --cluster add-node redis-slave-1:7101 redis-master-1:7001 --cluster-slave &&
+docker exec -it redis-master-1 redis-cli --cluster add-node redis-slave-2:7102 redis-master-2:7002 --cluster-slave &&
+docker exec -it redis-master-1 redis-cli --cluster add-node redis-slave-3:7103 redis-master-3:7003 --cluster-slave
