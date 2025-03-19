6fe699465df4dbc7318cda07cd4e447ad90cef45
kjeongh <wellsy001012@gmail.com>
2025-03-20 03:57:45 +0900

    prometheus 레이블 변경

--- cluster-create.zsh
+++ cluster-create.zsh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
 # Redis 클러스터 구성
+# docker-compose up 이후 클러스터 구성 스크립트를 실행합니다.
+
+# Master 구성
 docker exec -it redis-master-1 redis-cli --cluster create redis-master-1:7001 redis-master-2:7002 redis-master-3:7003 &&
+
 # Slave 노드 추가
 docker exec -it redis-master-1 redis-cli --cluster add-node redis-slave-1:7101 redis-master-1:7001 --cluster-slave &&
 docker exec -it redis-master-1 redis-cli --cluster add-node redis-slave-2:7102 redis-master-2:7002 --cluster-slave &&
